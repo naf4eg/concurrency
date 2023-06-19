660dbbeff0e9926c6c9765666edf09ac7527a38f
Konstantin <naf4eg@yandex.ru>
2023-06-19 12:28:45 +0300

    как зависят параметры экзекютора от количества задач

---
+++ src/main/java/course/concurrency/m0_intro/Main.java
@@ -0,0 +1,30 @@
+package course.concurrency.m0_intro;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * количество потоков выделяется не сразу, а по мере необходимости
+ */
+public class Main {
+    public static long longTask() throws InterruptedException {
+        Thread.sleep(1000); // + try-catch
+        return ThreadLocalRandom.current().nextInt();
+    }
+
+    public static void main(String[] args) {
+
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 60,
+                TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+
+        for (int i = 0; i < 10; i++) {
+            executor.submit(() -> longTask());
+            System.out.print(executor.getPoolSize() + " ");
+            System.out.println("=> " + executor.getQueue().remainingCapacity());
+        }
+
+        executor.shutdown();
+    }
+}
